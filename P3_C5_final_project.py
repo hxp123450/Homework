8974d2c511b45c9eb13ba57c6d5da75da564bbe5
hxp123450 <61919316+hxp123450@users.noreply.github.com>
2020-03-07 21:37:22 +0100

    Update P3_C5_final_project.py

--- P3_C5_final_project.py
+++ P3_C5_final_project.py
@@ -9,7 +9,7 @@ import numpy as np
 face_cascade = cv.CascadeClassifier('readonly/haarcascade_frontalface_default.xml')
 #openfile
 def text_face_rec(t_input):
-    print('''Processing...It may take a couple of minutes before you can see all the outcomes ''')
+    print('''Processing...It may take a couple of minutes before you can see all the outcomes.''')
     file = 'readonly/images.zip'
     z_file = zipfile.ZipFile(file, 'r')
     file_names = [file.filename for file in z_file.infolist()]
@@ -17,7 +17,14 @@ def text_face_rec(t_input):
         with z_file.open(file_names[i]) as myfile:
             img = Image.open(myfile)
             img = img.convert("RGB")
-#face recognition
+# word recogniton and output
+        t_str = pytesseract.image_to_string(img)
+        if '{}'.format(t_input.lower()) in t_str.lower():
+            print('"{}" found in {}'.format(t_input, file_names[i]))
+        else:
+            print('No text "{}" can be found in {}'.format(t_input, file_names[i]))
+
+# Face recognition
         c_faces = []
         faces = face_cascade.detectMultiScale(np.array(img), 1.3)
         drawing=ImageDraw.Draw(img)
@@ -29,25 +36,22 @@ def text_face_rec(t_input):
         contact_sheet = Image.new(img.mode, (200 * 5, 200 * 2))
         x = 0
         y = 0
-        first_image=c_faces[0]
-        for f in c_faces:
-            contact_sheet.paste(f, (x, y))
-            if x+first_image.width == contact_sheet.width:
-                x=0
-                y=y+first_image.height
-            else:
-                x=x+first_image.width
-# word recogniton and output
-        t_str = pytesseract.image_to_string(img)
-        if '{}'.format(t_input.lower()) in t_str.lower():
-            print('"{}" found in the article'.format(t_input))
-        else:
-            print('No text "{}" can be found in the article'.format(t_input))
         if len(c_faces) != 0:
             print('{} faces detected in {}'.format(len(c_faces), file_names[i]))
-        else:
+        try:
+            first_image=c_faces[0]
+            for f in c_faces:
+                contact_sheet.paste(f, (x, y))
+                if x+first_image.width == contact_sheet.width:
+                    x=0
+                    y=y+first_image.height
+                else:
+                    x=x+first_image.width
+            display(contact_sheet)
+        except:
             print('No faces found in {}'.format(file_names[i]))
-        display(contact_sheet)
+
+
 
 word = input("Please enter a word (e.g. Mark)\n")
 text_face_rec(word)
